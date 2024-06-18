922d17d86c8fba5bbcf64f48a1a76392a7e6132b
DKSHRI <devanshu.shrivastava@s.amity.edu>
2024-06-18 17:14:06 +0530

    18 06 24 update

---
+++ index.js
@@ -0,0 +1,8 @@
+function buttonchange(){
+if (document.querySelector(".buttonchange").innerHTML==='Subscribe') {
+    document.querySelector(".buttonchange").innerHTML='Subscribed'
+}
+else{
+     document.querySelector(".buttonchange").innerHTML='Subscribe'
+}
+}
